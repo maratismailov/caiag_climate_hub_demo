b9f2ed13bf63fc7fcd25737636f9298945cf49f3
Marat Ismailov <marat.ismailov889@gmail.com>
2021-09-16 22:12:17 +0600

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,40 @@
+<script>
+  import { Route, active } from "tinro";
+  import Location from "./components/Location.svelte";
+  import Photo from "./components/Photo.svelte";
+  import L1_1 from "./components/L1_1.svelte";
+  import L1_2 from "./components/L1_2.svelte";
+</script>
+
+<style>
+
+</style>
+
+<main>
+  <div class="scrollmenu">
+    <a href="/" use:active exact>L1_1</a>
+    <a href="/l1_2" use:active exact>L1_2</a>
+    <!-- <a href="/location" use:active exact>Location</a>
+    <a href="/photo" use:active exact>Photo</a> -->
+  </div>
+
+</main>
+
+<Route path="/photo">
+  <Photo />
+</Route>
+<Route path="/location">
+  <Location />
+</Route>
+<Route path="/">
+  <L1_1 />
+</Route>
+<Route path="/l1_2">
+  <L1_2 />
+</Route>
+<!-- <Route path="/currentdustazure">
+  <CurrentDustAzure />
+</Route>
+<Route path="/heatmap">
+  <Heatmap />
+</Route> -->
